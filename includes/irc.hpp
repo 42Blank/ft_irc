a25f494d467e5b82254112025fc0f57ecc2cc502
chichoon <chichoon.choi@gmail.com>
2022-12-29 15:55:16 +0900

    chore: irc.hpp

---
+++ includes/irc.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   irc.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/28 13:44:22 by jiychoi           #+#    #+#             */
+/*   Updated: 2022/12/28 18:37:48 by jiychoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IRC_HPP
+# define IRC_HPP
+
+#include <iostream>
+
+// 깃에 올리려고 만든 파일
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/un.h>
+
+#endif
