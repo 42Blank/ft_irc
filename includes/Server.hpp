d0edceeeb542d98e83638dcb18aae1ca7501e921
chichoon <chichoon.choi@gmail.com>
2022-12-30 03:24:09 +0900

    feat: 서버 메서드 추가
    - 클라이언트와 연결하는 메서드
    - 메시지 수신받아 \r\n 전까지 이어붙이는 메서드
    - 유저 데이터 파싱하는 메서드

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/29 16:35:50 by san               #+#    #+#             */
+/*   Updated: 2022/12/30 03:18:44 by jiychoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SERVER_HPP
 # define SERVER_HPP
 
@@ -14,26 +26,24 @@
 # define BUF_SIZE 1024
 
 class Server {
-private:
-	int		serv_sock;
-	char	msg[BUF_SIZE];
-	int		str_len;
+	private:
+		int					_server_socket;
+		struct sockaddr_in	_server_address;
+		std::vector<User *>	_user_vector;
+		char				_message[BUF_SIZE];
 
-	struct sockaddr_in	serv_adr;
-
-	std::vector<User *>	user_vector;
-
-	void	socketCreate(void);
-	void	errorHandling(std::string msg);
+		void		connectWithClient();
+		std::string	concatMessage(int client_socket);
+		void		parseUserData(User* user, const std::string& fullMsg);
 
 
-public:
-	Server();
-	Server(char *port);// 비번 추가 해야 함.
-	~Server();
+	public:
+		Server();
+		Server(char *port);// 비번 추가 해야 함.
+		~Server();
 
-	void	serverOn(void);
-	void	serverOff(void);
+		void	serverOn(void);
+		void	serverOff(void);
 
 };
 
--- src/Server.cpp
+++ src/Server.cpp
@@ -1,69 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/29 16:35:50 by san               #+#    #+#             */
+/*   Updated: 2022/12/30 03:19:25 by jiychoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/Server.hpp"
+#include "../includes/Error.hpp"
 
 Server::Server() {}
 
 Server::Server(char *port) {
-	this->serv_sock = socket(PF_INET, SOCK_STREAM, 0);	// 소켓 생성
-	if (this->serv_sock == -1)
-		errorHandling("socket() error");
+	_server_socket = socket(PF_INET, SOCK_STREAM, 0);	// 소켓 생성
+	if (_server_socket < 0)
+		throw Error::SocketOpenException();
 
-	memset(&this->serv_adr, 0, sizeof(this->serv_adr));	// 구조체 변수의 모든 멤버를 0으로 초기화
-	this->serv_adr.sin_family = AF_INET;			// 주소 체계 지정, AF_INET : IPv4 인터넷 프로토콜에 적용
-	this->serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);	// IP 주소
-	this->serv_adr.sin_port = htons(atoi(port));	// port 번호 초기화
+	memset(&_server_address, 0, sizeof(_server_address));	// 구조체 변수의 모든 멤버를 0으로 초기화
+	_server_address.sin_family = AF_INET;			// 주소 체계 지정, AF_INET : IPv4 인터넷 프로토콜에 적용
+	_server_address.sin_addr.s_addr = htonl(INADDR_ANY);	// IP 주소
+	_server_address.sin_port = htons(atoi(port));	// port 번호 초기화
 
-	if (bind(serv_sock, (struct sockaddr *)&this->serv_adr, sizeof(this->serv_adr)) == -1)	// 소켓 주소 할당
-		errorHandling("bind() error");
+	if (bind(_server_socket, (struct sockaddr *)&_server_address, sizeof(_server_address)) < 0)	// 소켓 주소 할당
+		throw Error::SocketOpenException();
 }
 
 Server::~Server() {
 	std::cout << "destructor called\n";
-	this->serverOff();
+	serverOff();
 }
 
 void	Server::serverOn(void) {
-	int client_socket;
-	std::string fullMsg;
-
-	if (listen(this->serv_sock, 5) == -1)	// 연결요청 대기상태
-		errorHandling("listen() error");
-
-	for (int i = 0; i < 5; i++) {
-		fullMsg = "";
-		User* user = new User();
-		client_socket = accept(this->serv_sock, (struct sockaddr*)user->getAddressPtr(), user->getAddressSizePtr());
-		if (client_socket < 0) {
-			delete user;
-			errorHandling("accept() error");
-		}
-		user->setSocketDesc(client_socket);
-		std::cout << "Client connected: " << i + 1 << "\n";
-		while ((this->str_len = recv(user->getSocketDesc(), this->msg, BUF_SIZE, 0)) != 0) {
-			if (this->str_len == -1) continue;
-			this->msg[this->str_len] = 0;
-			fullMsg += this->msg;
-			if (fullMsg.substr(fullMsg.length() - 2) == "\r\n") break;
-		}
-
-		fullMsg.find("NICK");
-
-		this->user_vector.push_back(user); // 유저 검증 성공했을 때만 push back (실패하면 메모리 해제해야함)
-		std::cout << "Message: " << fullMsg << "\n";
-		// close(user->getSocketDesc());
-	}
-}
-
-void	Server::errorHandling(std::string msg) {
-
-	std::cout << "ircserv: " << msg << '\n';
-	serverOff();
-	exit(1);
+	if (listen(_server_socket, 5) < 0)	// 연결요청 대기상태
+		throw Error::SocketOpenException();
+	connectWithClient();
 }
 
 void	Server::serverOff(void) {
 	std::vector<User *>::iterator	iter;
-	for (iter = this->user_vector.begin(); iter != this->user_vector.end(); iter++)
+	for (iter = _user_vector.begin(); iter != _user_vector.end(); iter++)
 		delete (*iter);
-	close(this->serv_sock);
+	close(_server_socket);
 }
 
+void	Server::connectWithClient() {
+	int			client_socket;
+
+	while (1) {
+		try {
+			User* user = new User();
+			client_socket = accept(_server_socket, (struct sockaddr*)user->getAddressPtr(), user->getAddressSizePtr());
+			if (client_socket < 0) {
+				delete user;
+				throw Error::SocketOpenException();
+			}
+			user->setSocketDesc(client_socket);
+			// std::cout << "Client connected: " << i + 1 << "\n";
+
+			std::string fullMsg = concatMessage(user->getSocketDesc());
+			parseUserData(user, fullMsg);
+
+		_user_vector.push_back(user); // 유저 검증 성공했을 때만 push back (실패하면 메모리 해제해야함)
+		std::cout << "Message: " << fullMsg << "\n";
+		// close(user->getSocketDesc());
+		} catch (std::exception &e) {
+			std::cout << e.what() << "\n";
+			continue;
+		}
+	}
+}
+
+std::string	Server::concatMessage(int client_socket) {
+	int		message_length;
+	std::string	fullMsg = "";
+
+	while ((message_length = recv(client_socket, _message, BUF_SIZE, 0)) != 0) {
+		if (message_length < 0) continue;
+		_message[message_length] = 0;
+		fullMsg += _message;
+		if (fullMsg.length() >= 2 && fullMsg.substr(fullMsg.length() - 2) == "\r\n") break;
+	}
+	return fullMsg;
+}
+
+void		Server::parseUserData(User* user, const std::string& fullMsg) {
+	size_t pos = fullMsg.find("NICK");
+	std::cout << "NICK position: [" << pos << "]\n";
+	std::cout << "NICK: [" << fullMsg.at(pos) << "\\n";
+	std::cout << "User [" << user << "]\n";
+}
