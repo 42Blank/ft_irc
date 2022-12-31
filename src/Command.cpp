c62b3d1bc90466cfa5a1bc811e207e0882681ab8
chichoon <chichoon.choi@gmail.com>
2022-12-31 15:21:08 +0900

    chore: 유저명 중복검사 삭제
    - 필요 X

--- src/Command.cpp
+++ src/Command.cpp
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 04:09:31 by jiychoi           #+#    #+#             */
-/*   Updated: 2022/12/31 15:07:55 by jiychoi          ###   ########.fr       */
+/*   Updated: 2022/12/31 15:20:25 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,8 @@ void	Server::commandNICK(User* user, std::vector<std::string>& parameters) {
 }
 
 void	Server::commandUser(User* user, std::vector<std::string>& parameters) {
-	std::vector<User>::iterator	iter;
 	const std::string username = *(parameters.end() - 1);
 	user->setUsername(username);
 
-	for (iter = _user_vector.begin(); iter < _user_vector.end(); iter++) {
-		if ((*iter).getUsername() == username)
-			throw Error::AuthorizeException();
-	}
 	_user_vector.push_back(*user);
 }
