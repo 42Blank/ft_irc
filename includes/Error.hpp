c2492da206b4d09b3b07148f2366f628db12c87f
chichoon <chichoon.choi@gmail.com>
2023-01-03 01:06:14 +0900

    chore: 서버에서 사용하는 예외들 추가
    - 노가다,,,,

--- includes/Error.hpp
+++ includes/Error.hpp
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 19:30:34 by jiychoi           #+#    #+#             */
-/*   Updated: 2023/01/03 00:16:31 by jiychoi          ###   ########.fr       */
+/*   Updated: 2023/01/03 01:04:40 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,20 @@
 # include "irc.hpp"
 # include <string>
 
-# define ERR_NONICKNAMEGIVEN	"431" // No nickname given
-# define ERR_ERRONEUSNICKNAME	"432" // Erroneous nickname
-# define ERR_NICKNAMEINUSE		"433" // Nickname is already in use
-# define ERR_USERNOTINCHANNEL	"441" // You're not on that channel
-# define ERR_NOTONCHANNEL		"442" // They aren't on that channel
-# define ERR_NOTREGISTERED		"451" // You have not registered
-# define ERR_NEEDMOREPARAMS		"461" // Not enough parameters"
-# define ERR_PASSWDMISMATCH		"464" // Password incorrect
+# define ERR_UNKNOWNCOMMAND			"421" // Unknown command
+# define ERR_NONICKNAMEGIVEN		"431" // No nickname given
+# define ERR_ERRONEUSNICKNAME		"432" // Erroneous nickname
+# define ERR_NICKNAMEINUSE			"433" // Nickname is already in use
+# define ERR_USERNOTINCHANNEL		"441" // You're not on that channel
+# define ERR_NOTONCHANNEL			"442" // They aren't on that channel
+# define ERR_NOTREGISTERED			"451" // You have not registered
+# define ERR_NEEDMOREPARAMS			"461" // Not enough parameters"
+# define ERR_PASSWDMISMATCH			"464" // Password incorrect
+
+# define ERR_SERVEROPENFAILED		"600" // Failed to open server
+# define ERR_CLIENTCONNECTFAILED	"601" // Failed to connect with client
+# define ERR_CANNOTFINDUSERFD		"602" // Cannot find user by fd
+# define ERR_MESSAGESENDFAILED		"603" // Failed to send message
 
 class Error: public std::exception {
 	private:
@@ -36,6 +42,7 @@ class Error: public std::exception {
 		Error(std::string errCode);
 		Error(std::string errCode, std::string param1);
 		Error(std::string errCode, std::string param1, std::string param2);
+		~Error() throw();
 		const char* what() const throw();
 };
 
--- src/Error.cpp
+++ src/Error.cpp
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 19:31:32 by jiychoi           #+#    #+#             */
-/*   Updated: 2023/01/03 00:19:09 by jiychoi          ###   ########.fr       */
+/*   Updated: 2023/01/03 01:06:09 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ Error::Error(std::string errCode, std::string param1):
 Error::Error(std::string errCode, std::string param1, std::string param2):
 	_errCode(errCode), _param1(param1), _param2(param2) {}
 
+Error::~Error() throw() {}
+
 const char* Error::what() const throw() {
 	if (_param1.length() == 0)
 		return (_errCode + getErrorMessage(_errCode)).c_str();
@@ -30,6 +32,18 @@ const char* Error::what() const throw() {
 }
 
 std::string	Error::getErrorMessage(std::string code) const {
+	if (code == ERR_UNKNOWNCOMMAND) return " :Unknown command";
+	if (code == ERR_NONICKNAMEGIVEN) return " :No nickname given";
+	if (code == ERR_ERRONEUSNICKNAME) return " :Erroneous nickname";
+	if (code == ERR_NICKNAMEINUSE) return " :Nickname is already in use";
+	if (code == ERR_USERNOTINCHANNEL) return " :You're not on that channel";
+	if (code == ERR_NOTONCHANNEL) return " :They aren't on that channel";
+	if (code == ERR_NOTREGISTERED) return " :You have not registered";
 	if (code == ERR_NEEDMOREPARAMS) return " :Not enough parameters";
 	if (code == ERR_PASSWDMISMATCH) return " :Password incorrect";
+
+	if (code == ERR_SERVEROPENFAILED) return " :Failed to open server";
+	if (code == ERR_CLIENTCONNECTFAILED) return " :Failed to connect with client";
+	if (code == ERR_CANNOTFINDUSERFD) return " :Cannot find user by fd";
+	if (code == ERR_MESSAGESENDFAILED) return " :Failed to send message";
 }
