4b356d6a9605c4d4fd62f72da289ca0113531290
chichoon <chichoon.choi@gmail.com>
2023-01-03 00:20:20 +0900

    feat: Error 함수 대격변
    - 생성자 3종류 (추가 패러미터 0, 1, 2개)
    - 패러미터를 넣어서 에러 문구 다르게 출력 가능
    - 에러 코드 상수화, 에러문구 함수화 (매크로상수 배열지정이 안됨..)

--- includes/Error.hpp
+++ includes/Error.hpp
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 19:30:34 by jiychoi           #+#    #+#             */
-/*   Updated: 2023/01/02 22:16:37 by jiychoi          ###   ########.fr       */
+/*   Updated: 2023/01/03 00:16:31 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,29 @@
 # define ERROR_HPP
 
 # include "irc.hpp"
+# include <string>
 
-class Error {
+# define ERR_NONICKNAMEGIVEN	"431" // No nickname given
+# define ERR_ERRONEUSNICKNAME	"432" // Erroneous nickname
+# define ERR_NICKNAMEINUSE		"433" // Nickname is already in use
+# define ERR_USERNOTINCHANNEL	"441" // You're not on that channel
+# define ERR_NOTONCHANNEL		"442" // They aren't on that channel
+# define ERR_NOTREGISTERED		"451" // You have not registered
+# define ERR_NEEDMOREPARAMS		"461" // Not enough parameters"
+# define ERR_PASSWDMISMATCH		"464" // Password incorrect
+
+class Error: public std::exception {
+	private:
+		std::string		_errCode;
+		std::string		_param1;
+		std::string		_param2;
+
+		std::string		getErrorMessage(std::string code) const;
 	public:
-		class SocketOpenException: public std::exception {
-			public:
-				const char* what() const throw();
-		};
-		class ParseMessageException: public std::exception {
-			public:
-				const char* what() const throw();
-		};
-		class AuthorizeException: public std::exception {
-			public:
-				const char* what() const throw();
-		};
-		class SendMessageException: public std::exception {
-			public:
-				const char* what() const throw();
-		};
-		class UserNotFoundException: public std::exception {
-			public:
-				const char* what() const throw();
-		};
-		class PasswordException: public std::exception {
-			public:
-				const char* what() const throw();
-		};
-		class InvalidCommandException: public std::exception {
-			public:
-				const char* what() const throw();
-		};
+		Error(std::string errCode);
+		Error(std::string errCode, std::string param1);
+		Error(std::string errCode, std::string param1, std::string param2);
+		const char* what() const throw();
 };
 
 #endif
--- src/Error.cpp
+++ src/Error.cpp
@@ -6,37 +6,30 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 19:31:32 by jiychoi           #+#    #+#             */
-/*   Updated: 2023/01/02 22:16:17 by jiychoi          ###   ########.fr       */
+/*   Updated: 2023/01/03 00:19:09 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../includes/Error.hpp"
 
-const char* Error::SocketOpenException::what() const throw() {
-	return "Socket Open Failed";
+Error::Error(std::string errCode):
+	_errCode(errCode), _param1(""), _param2("") {}
+
+Error::Error(std::string errCode, std::string param1):
+	_errCode(errCode), _param1(param1), _param2("") {}
+
+Error::Error(std::string errCode, std::string param1, std::string param2):
+	_errCode(errCode), _param1(param1), _param2(param2) {}
+
+const char* Error::what() const throw() {
+	if (_param1.length() == 0)
+		return (_errCode + getErrorMessage(_errCode)).c_str();
+	if (_param2.length() == 0)
+		return (_errCode + " " + _param1 + getErrorMessage(_errCode)).c_str();
+	return (_errCode + " " + _param1 + " " + _param2 + getErrorMessage(_errCode)).c_str();
 }
 
-const char*	Error::ParseMessageException::what() const throw() {
-	return "No Nickname Sent";
+std::string	Error::getErrorMessage(std::string code) const {
+	if (code == ERR_NEEDMOREPARAMS) return " :Not enough parameters";
+	if (code == ERR_PASSWDMISMATCH) return " :Password incorrect";
 }
-
-const char*	Error::AuthorizeException::what() const throw() {
-	return "Authorization Failed";
-}
-
-const char*	Error::SendMessageException::what() const throw() {
-	return "Failed to send message to client";
-}
-
-const char*	Error::UserNotFoundException::what() const throw() {
-	return "Cannot find user";
-}
-
-const char*	Error::PasswordException::what() const throw() {
-	return "Wrong Password or invalid parameter";
-}
-
-const char*	Error::InvalidCommandException::what() const throw() {
-	return "Invalid Command";
-}
-
