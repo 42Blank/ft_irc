dbbe62c93fec7f0c2208013c9438359f2c575d5e
chichoon <chichoon.choi@gmail.com>
2023-01-03 02:36:03 +0900

    fix: 닉네임 리스트 형식에 맞게 수정

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,7 +1,7 @@
 
 #include "../includes/Channel.hpp"
 
-// 여기 유저 nickname만 받아서 Channel user list에 넣으면 안될지 고민 중 
+// 여기 유저 nickname만 받아서 Channel user list에 넣으면 안될지 고민 중
 Channel::Channel(User &user, std::string name) {
 	_operator = user.getNickname();
 	_userList.push_back(user);
@@ -17,16 +17,14 @@ std::string			Channel::getTopic() {
 }
 
 // 그냥 유저 닉네임을 모두 출력하도록 함
-std::string			Channel::getuserList() {
-	
+std::string			Channel::getUserList() {
+
 	std::string	userList;
 	std::vector<User>::iterator	iter;
 
-	userList = _channelName + " operator is " + _operator + "\nUserList [ ";
-	for (iter = _userList.begin(); iter < _userList.end(); iter++) {
-		userList = userList + (*iter).getNickname() + " ";
-	}
-	userList = userList + "]";
+	userList = "= #" + _channelName + " :@" + _operator;
+	for (iter = _userList.begin(); iter < _userList.end(); iter++)
+		userList += " " + (*iter).getNickname();
 	return userList;
 }
 
@@ -38,12 +36,12 @@ void				Channel::setTopic(std::string topic) {
 	_topic = topic;
 }
 
-// 새로운 유저가 조인하면 이 메서드를 통해서 채널의 userList에 추가한다. 
+// 새로운 유저가 조인하면 이 메서드를 통해서 채널의 userList에 추가한다.
 void	Channel::joinNewUser(User user) {
 	_userList.push_back(user);
 }
 
-// 관리자인지 확인하는 메서드 
+// 관리자인지 확인하는 메서드
 bool	Channel::isOperator(User user) {
 	if (user.getNickname().compare(_operator) == 0) {
 		return true;
@@ -61,5 +59,5 @@ void	Channel::deleteNormalUser(std::string nickname){
 }
 
 // void	Channel::deleteOperatorUser(std::string nickname) {
-	
+
 // }
