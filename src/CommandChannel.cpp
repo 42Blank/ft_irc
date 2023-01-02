b3dd89850e3cb892a577c027bfa9ae2e50f57acb
anso33 <hyeon131812@gmail.com>
2023-01-02 16:55:58 +0900

    feat: JOIN 명령어 추가

--- src/CommandChannel.cpp
+++ src/CommandChannel.cpp
@@ -1,16 +1,57 @@
 
 
 #include "../includes/Server.hpp"
+#include "../includes/Reply.hpp"
 
-bool		Server::isChannel(std::string param) {
-	if (param[0] != '&' && param[0] != '#' && param[0] != '+' && param[0] != '!')
+
+
+
+void	Server::commandJOIN(User *user, std::vector<std::string> &parameters) {
+
+	//ERR_NEEDMOREPARAMS (461)
+	if (parameters.size() < 2) throw Error::AuthorizeException(); // TODO: 다른 오류(파라미터 부족)로 교체
+
+	if (isChannel(parameters[1])) {
+		//채널에 가입된다. 
+		findChannel(parameters[1]).joinNewUser(*user);
+		sendClientMessage(user, user->getNickname() + " JOIN " + parameters[1]);
+		Channel ch = findChannel(parameters[1]);
+		if (ch.getTopic() == "")
+			sendClientMessage(user, RPL_NOTOPIC(ch.getChannelName()));
+		else
+		sendClientMessage(user, RPL_TOPIC(ch.getChannelName(), ch.getTopic()));
+		sendClientMessage(user, RPL_NAMREPLY(ch.getChannelName(), ch.getuserList()));
+		sendClientMessage(user, RPL_ENDOFNAMES(ch.getChannelName()));
+	} else {
+		Channel channel = Channel(*user, parameters[1]);
+		_channelList.push_back(channel);
+		sendClientMessage(user, RPL_NOTOPIC(channel.getChannelName()));
+		sendClientMessage(user, RPL_NAMREPLY(channel.getChannelName(), channel.getuserList()));
+		sendClientMessage(user, RPL_ENDOFNAMES(channel.getChannelName()));
+	}
+
+}
+
+Channel		Server::findChannel(std::string channelName) {
+	std::vector<Channel>::iterator	iterChannel;
+
+	for (iterChannel = _channelList.begin(); iterChannel < _channelList.end(); iterChannel++) {
+		if ((*iterChannel).getChannelName().compare(channelName) == 0) {
+			return *iterChannel;
+		}
+	}
+	throw Error::AuthorizeException(); // 채널이 없다는 에러 메세지로 바꾸기 
+}
+
+bool		Server::isChannel(std::string channelName) {
+	if (channelName[0] != '&' && channelName[0] != '#' && channelName[0] != '+' && channelName[0] != '!')
 		return false;
 
-	// param = param.substr(1, param.npos);
+	// channelName = channelName.substr(1, channelName.npos);
 	//순회하면서 해당 이름 채널이 있는지 확인 
 	std::vector<Channel>::iterator	iter;
 	for (iter = _channelList.begin(); iter < _channelList.end(); iter++) {
-		if ((*iter).getChannelName().compare(param) == 0)
+		if ((*iter).getChannelName().compare(channelName) == 0)
 			return true;
 	}
 	return false;
@@ -39,15 +80,15 @@ void		Server::commandTOPIC(User* user, std::vector<std::string>& parameters) {
 	
 }
 
-void	Server::commandNICK(User* user, std::vector<std::string>& parameters) {
+// void	Server::commandNAMES(User* user, std::vector<std::string>& parameters) {
+	
+// }
 
-}
-
-void	Server::commandUser(User* user, std::vector<std::string>& parameters) {
+// void	Server::commandUser(User* user, std::vector<std::string>& parameters) {
 
 
-	sendClientMessage(user,
-		":127.0.0.1 001 " + user->getNickname() + " :\033[1;32mWelcome to the " + SERVER_NAME + "\e[0m " + \
-		user->getNickname() + "!" + user->getUsername() + "@" + user->getHostname()
-	);
-}
+// 	sendClientMessage(user,
+// 		":127.0.0.1 001 " + user->getNickname() + " :\033[1;32mWelcome to the " + SERVER_NAME + "\e[0m " + \
+// 		user->getNickname() + "!" + user->getUsername() + "@" + user->getHostname()
+// 	);
+// }
