d6526708d9f0411c78a697b9af9d832249eade1d
chichoon <chichoon.choi@gmail.com>
2023-01-04 16:59:39 +0900

    feat: QUIT 커맨드 추가

--- src/Command.cpp
+++ src/Command.cpp
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 04:09:31 by jiychoi           #+#    #+#             */
-/*   Updated: 2023/01/03 02:50:04 by jiychoi          ###   ########.fr       */
+/*   Updated: 2023/01/04 16:51:33 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,12 @@ void	Server::commandPONG(User& user, std::vector<std::string>& parameters) {
 	if (!(user.getIsVerified() != ALL_VERIFIED)) throw std::runtime_error(Error(ERR_NOTREGISTERED));
 	if (parameters.size() < 2) throw std::runtime_error(Error(ERR_NOORIGIN));
 	else if (parameters.size() > 2) throw std::runtime_error(Error(ERR_NOSUCHSERVER, parameters[1]));
+}
+
+void	Server::commandQUIT(User& user, std::vector<std::string>& parameters) {
+	std::string quitUserHostname = user.getHostname();
+	std::string quitUserNickname = user.getNickname();
+	std::string	quitMsg = ft_getStringAfterColon(parameters);
+	sendClientMessage2(user, ErrorReply(":Closing Link:", quitUserHostname, "(Quit: " + quitUserNickname + ")"));
+	user.setIsDisconnected(true);
 }
