65d4a3dd431f697ae0cf6848e4a280a413bd6cf8
jasong <jasong@student.42seoul.kr>
2023-01-06 01:09:24 +0900

    fix: server 초기화 try catch로 넣음

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: jasong <jasong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 13:45:58 by jiychoi           #+#    #+#             */
-/*   Updated: 2023/01/02 22:33:16 by jiychoi          ###   ########.fr       */
+/*   Updated: 2023/01/06 01:08:52 by jasong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,8 @@ int main(int argc, char* argv[]) {
 	}
 	std::cout << "oo                                                       \n                                                         \ndP 88d888b. .d8888b. .d8888b. .d8888b. 88d888b. dP   .dP \n88 88'  `88 88'  `'' Y8ooooo. 88ooood8 88'  `88 88   d8' \n88 88       88.  ...       88 88.  ... 88       88 .88'  \ndP dP       `88888P' `88888P' `88888P' dP       8888P'   \n                                                       \n";
 
-	Server server =  Server(argv[1], argv[2]);
-
 	try {
+		Server server = Server(argv[1], argv[2]);
 		server.serverOn();
 	} catch (std::exception &e) {
 		std::cout << "ircserv: Error: " << e.what() << "\n";
