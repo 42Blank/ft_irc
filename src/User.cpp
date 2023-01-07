6e91857bd5238e289be75036633de23627d107de
chichoon <chichoon.choi@gmail.com>
2023-01-07 16:06:28 +0900

    chore: 출력문 약간 변경

--- src/User.cpp
+++ src/User.cpp
@@ -6,7 +6,7 @@
 /*   By: jiychoi <jiychoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 03:50:34 by jiychoi           #+#    #+#             */
-/*   Updated: 2023/01/07 16:04:43 by jiychoi          ###   ########.fr       */
+/*   Updated: 2023/01/07 16:06:19 by jiychoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ User::User(void) {
 	_clientAddressSize = new socklen_t(sizeof(_clientAddress));
 	_isVerified = 0b000;
 	_isDisconnected = false;
-	std::cout << *this << " user created\n";
+	std::cout << "User created (" << this << ")\n";
 }
 
 User::~User(void) {
 	delete _clientAddress;
 	delete _clientAddressSize;
-	std::cout << *this << " user deleted\n";
+	std::cout << *this << " User deleted (" << this << ")\n";
 }
 
 User::User(const User& instance) {
@@ -39,7 +39,7 @@ User&	User::operator=(const User& instance) {
 	_hostname = instance.getHostname();
 	_isDisconnected = instance.getIsDisconnected();
 	_isVerified = instance.getIsVerified();
-	std::cout << *this << " user copied\n";
+	std::cout << *this << " user copied (" << this << ")\n";
 	return *this;
 }
 
