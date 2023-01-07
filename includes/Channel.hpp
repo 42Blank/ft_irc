ae12b06df1f0ea18066df0e488aad0b52d98ba41
anso33 <hyeon131812@gmail.com>
2023-01-08 04:33:50 +0900

    fix: mode를 bit로 관리하도록 수정

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -15,33 +15,52 @@
 
 # include "User.hpp"
 
+# define MODE_O 0x01
+# define MODE_P 0x02
+# define MODE_S 0x04
+# define MODE_I 0x08
+# define MODE_T 0x10
+# define MODE_N 0x20
+# define MODE_M 0x40
+# define MODE_L 0x80
+
 class Channel {
 	private:
-		std::string	_channelName;
-		userVector	_channelOperator;	// user의 nickname 저장
-		userVector	_channelUser;
-		std::string	_channelTopic;
-		std::string	_channelMode;
+		std::string		_channelName;
+		userVector		_channelOperator;	// user의 nickname 저장
+		userVector		_channelUser;
+		std::string		_channelTopic;
+		unsigned char	_channelMode;
+		bool			_isDeleted;
 
 	public:
 		Channel(User* user, std::string name);
 		~Channel();
 
-		void		joinNewUser(User* user);
-		bool		isOperator(std::string nickname);
-		bool		isUserInChannel(std::string nickname);
+		void			joinNewUser(User* user);
+		void			joinNewOperator(User* user);
+		bool			isOperator(std::string nickname);
+		bool			isUserInChannel(std::string nickname);
 
-		std::string	getUserList();
-		userVector	getOperatorVector();
-		userVector	getNormalUserVector();
-		std::string	getChannelName();
-		std::string	getChannelMode();
+		std::string		getUserList();
+		userVector		getOperatorVector();
+		userVector		getNormalUserVector();
+		std::string		getChannelName();
+		std::string		getChannelModeToStr();
+		unsigned char	getChannelMode();
+		bool			getIsDeleted();
 
-		void		setTopic(std::string topic);
-		void		setChannelMode(std::string channelMode);
+		std::string		modeBitToStr(unsigned char mode);
+		unsigned char	modeStrToBit(char m);
+		void			changeChannelMode(Channel *ch, std::string &mode);
 
-		void		deleteNormalUser(std::string nickname);
-		int			deleteOperatorUser(std::string nickname);
+
+		void			setTopic(std::string topic);
+		void			setChannelMode(unsigned char channelMode);
+		void			setIsDeleted(bool isDeleted);
+
+		void			deleteNormalUser(std::string nickname);
+		int				deleteOperatorUser(std::string nickname);
 
 };
 
